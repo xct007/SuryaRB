f4023ad6af13a6a7fed6f980fca2bcf2310f621b
Nathajana <35220738+nat9h@users.noreply.github.com>
2024-02-17 15:13:51 +0800

    add bing image

---
+++ Message/Features/bingimage.js
@@ -0,0 +1,42 @@
+// File: /home/rose/BOT/SuryaRB/Message/Features/bingimage.js
+export default {
+	command: ["bingimg", "createimg"],
+	description: "Generate Image use Bing.",
+	category: "Image",
+	owner: false,
+	admin: false,
+	hidden: false,
+	limit: 0,
+	group: false,
+	private: false,
+
+	execute: async function (m, { sock, api, text }) {
+		if (!text) {
+			await m.reply("Please provide a prompt.");
+			return;
+		}
+		const { data } = await api.post("/image/bing_create_image", {
+			prompt: text,
+		});
+
+		const { status, result, message } = data;
+
+		if (!status) {
+			throw new Error(message);
+		}
+
+		const { images } = result;
+
+		const sendMessagePromises = images.map(async (url) => {
+			await sock.sendMessage(m.chat, { image: { url: url } }, { quoted: m });
+		});
+
+		await Promise.all(sendMessagePromises); // for waiting images
+	},
+	
+	failed: "Failed to execute the %cmd command\n%error",
+	wait: ["Please wait %tag", "Hold on %tag, fetching response"],
+	done: null,
+};
+
+// Path: Message/Features/bingimage.js
