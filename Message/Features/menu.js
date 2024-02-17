b163b707c58087bfafd96879724d7fe4e5fb9fe0
FrierenDv <hackerxct007@gmail.com>
2024-02-17 12:07:47 +0700

    refactor: change the way the menu is displayed

--- Message/Features/menu.js
+++ Message/Features/menu.js
@@ -3,7 +3,7 @@ import Feature from "../Feature.js";
 
 export default {
 	command: ["menu"],
-	description: "Main Menu",
+	description: "Show this menu",
 	category: "Main",
 	owner: false,
 	admin: false,
@@ -13,30 +13,33 @@ export default {
 	private: false,
 
 	execute: async function (m, { sock, usedPrefix, isOwner, isAdmin }) {
-		// do good enough, but yeah we'll try
 		const features = Feature.plugins;
-		const filterdHidden = {};
-		for (const x in features) {
-			if (!features[x].hidden) {
-				filterdHidden[x] = features[x];
-			}
-		}
-		const plugins = {};
-		for (const p in filterdHidden) {
-			const categorie = filterdHidden[p]?.category?.trim() || "Unknown";
-			if (!plugins[categorie]) {
-				plugins[categorie] = [];
-			}
-			plugins[categorie].push(filterdHidden[p]);
-		}
+		const filterdHidden = Object.fromEntries(
+			Object.entries(features).filter(([_, feature]) => !feature.hidden)
+		);
+		const plugins = Object.entries(filterdHidden).reduce((acc, [key, value]) => {
+			const category = value.category?.trim() || "Unknown";
+			acc[category] = acc[category] || [];
+			acc[category].push(value);
+			return acc;
+		}, {});
 		const categories = Object.keys(plugins).sort();
 		let message = "";
 		for (const category of categories) {
-			message += `-\`${category}\`\n`;
+			message += `\`\`\`[ ${category} ]\`\`\`\n`;
 			for (const plugin of plugins[category]) {
-				message += `${usedPrefix}${
-					Array.isArray(plugin.command) ? plugin.command[0] : plugin.command
-				}\n`;
+				const command = Array.isArray(plugin.command)
+					? plugin.command[0]
+					: plugin.command;
+				const aliases =
+					(Array.isArray(plugin.command) ? plugin.command.slice(1).join(", ") : null) ||
+					"-";
+				message +=
+					((plugin.owner && !isOwner) || (plugin.admin && !isAdmin)
+						? `~${usedPrefix + command}~`
+						: `\`${usedPrefix + command}\``) + "\n";
+				message += `↳ ${plugin.description}\n`;
+				message += `↳ Aliases: ${aliases}\n`;
 			}
 		}
 		m.reply(message.trim());
