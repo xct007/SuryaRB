354d3a50b2e139b7110f0242f11332c95453b72e
FrierenDv <hackerxct007@gmail.com>
2024-02-17 12:11:38 +0700

    add: Libs/Uploader.js add: Message/Features/upload.js

---
+++ Libs/Uploader.js
@@ -0,0 +1,16 @@
+import axios from "axios";
+
+export const telegraph = async (buffer) => {
+	if (!Buffer.isBuffer(buffer)) {
+		throw new Error("Buffer is not a buffer");
+	}
+	const form = new FormData();
+    const blob = new Blob([buffer], { type: "image/jpeg" });
+	form.append("file", blob, "file");
+	const { data } = await axios.post("https://telegra.ph/upload", form, {
+		headers: {
+			"Content-Type": "multipart/form-data",
+		},
+	});
+	return "https://telegra.ph" + data[0].src;
+};
---
+++ Message/Features/upload.js
@@ -0,0 +1,27 @@
+// File://home/rose/BOT/SuryaRB/Message/Features/upload.js
+import { telegraph } from "../../Libs/Uploader.js";
+
+export default {
+	command: ["upload", "tourl"],
+	description: "Upload a file to telegra.ph",
+	category: "Utility",
+	owner: false,
+	admin: false,
+	hidden: false,
+	limit: 0,
+
+	execute: async function (m, { sock }) {
+		const q = m.quoted ? m.quoted : m;
+		const mime = q.mtype || "";
+		if (!/webp|image|video|webm/g.test(mime)) {
+			return m.reply("Please reply/send a image with the command");
+		}
+		const media = await q.download();
+		const buffer = Buffer.isBuffer(media) ? media : Buffer.from(media, "utf-8");
+		const url = await telegraph(buffer);
+		m.reply(url);
+	},
+	failed: "Failed to execute the %cmd command\n%error",
+	wait: null,
+	done: null,
+};
