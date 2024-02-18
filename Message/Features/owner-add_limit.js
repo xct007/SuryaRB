fbe1c620f9c802c849a1ffde8376768cb83ffa01
FrierenDv <hackerxct007@gmail.com>
2024-02-18 16:28:08 +0700

    Add: Message/Features/owner-add_limit.js

---
+++ Message/Features/owner-add_limit.js
@@ -0,0 +1,44 @@
+// File: Message/Features/character-ai.js
+export default {
+	command: ["limit"],
+	description: "Add or reduce the limit of a user",
+	category: "Owner",
+	owner: true,
+	admin: false,
+	hidden: false,
+	limit: 0,
+	group: false,
+	private: false,
+
+	/**
+	 * @param {import("../../Utils/Messages").ExtendedWAMessage} m - The message object.
+	 * @param {import("../Handler").miscOptions}
+	 */
+	execute: async function (m, { sock, db, args }) {
+		const who = m.quoted?.sender || m.mentionedJid[0] || "";
+		const [cmd, number] = args.filter((x) => "@" + x !== who && !x.startsWith("@"));
+		console.log({
+			who,
+			cmd,
+			number,
+		});
+		const user = db.users.get(who);
+		if (!user) {
+			return m.reply("User not found");
+		}
+		if (isNaN(number)) {
+			return m.reply("Invalid number");
+		}
+		if (cmd === "add") {
+			user.limit += Number(number);
+			return m.reply(`Success added ${number} to ${who}'s limit`);
+		} else if (cmd == "reduce") {
+			user.limit -= Number(number);
+			return m.reply(`Success reduced ${number} from ${who}'s limit`);
+		}
+	},
+
+	failed: "Failed to execute the %cmd command\n\n%error",
+	wait: null,
+	done: null,
+};
