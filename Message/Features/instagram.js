4b930aa643e6918669d6e2dead98d878086a00ba
FrierenDv <hackerxct007@gmail.com>
2024-02-18 09:47:32 +0700

    add: Message/Features/instagram.js

---
+++ Message/Features/instagram.js
@@ -0,0 +1,48 @@
+// File://home/rose/BOT/SuryaRB/Message/Features/tiktok.js
+import axios from "axios";
+
+export default {
+	command: ["instagram", "ig"],
+	description: "Download Media from Instagram.",
+	category: "Downloader",
+	owner: false,
+	admin: false,
+	hidden: false,
+	limit: 0,
+	group: false,
+	private: false,
+
+	execute: async function (m, { sock, api, args }) {
+		const url = args[0];
+		if (!url) {
+			return m.reply("Please provide a Instagram link");
+		}
+		const { data } = await api.get("/downloader/ig", { url });
+		const { status, message, result } = data;
+		if (!status) {
+			return m.reply(message);
+		}
+		const contentType = async (url) =>
+			axios
+				.head(url)
+				.then((res) => res.headers["content-type"])
+				.catch(() => null);
+		for (const obj of result) {
+			const type = await contentType(obj.url);
+			if (!type) {
+				continue;
+			}
+			await sock.sendMessage(
+				m.chat,
+				{
+					[type.includes("video") ? "video" : type.includes("audio") ? "audio" : "image"]:
+						{ url: obj.url },
+				},
+				{ quoted: m }
+			);
+		}
+	},
+	failed: "Failed to execute the %cmd command\n%error",
+	wait: null,
+	done: null,
+};
