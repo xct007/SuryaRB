c0d99fffb3a4eebe7fa2f4e78cd0284c34a8c803
FrierenDv <hackerxct007@gmail.com>
2024-02-19 10:09:47 +0700

    Fonts

---
+++ Config/Fonts.js
@@ -0,0 +1,15 @@
+export const system = "abcdefghijklmnopqrstuvwxyz";
+export const fonts = {
+	acuteAccent: "ábćdéfǵhíjḱĺḿńőṕqŕśtúvẃxӳź",
+	crypticItalic: "𐌀𐌁𐌂𐌃𐌄𐌅Ᏽ𐋅𐌉Ꮭ𐌊𐌋𐌌𐌍Ꝋ𐌐𐌒𐌓𐌔𐌕𐌵ᕓᏔ𐋄𐌙Ɀ",
+	fraktur: "𝔞𝔟𝔠𝔡𝔢𝔣𝔤𝔥𝔦𝔧𝔨𝔩𝔪𝔫𝔬𝔭𝔮𝔯𝔰𝔱𝔲𝔳𝔴𝔵𝔶𝔷",
+	funky: "αв¢∂єƒgнιנкℓмησρqяѕтυνωχуz",
+	handWriting: "𝒶𝒷𝒸𝒹𝑒𝒻𝑔𝒽𝒾𝒿𝓀𝓁𝓂𝓃𝑜𝓅𝓆𝓇𝓈𝓉𝓊𝓋𝓌𝓍𝓎𝓏",
+	lightBublles: "ⓐⓑⓒⓓⓔⓕⓖⓗⓘⓙⓚⓛⓜⓝⓞⓟⓠⓡⓢⓣⓤⓥⓦⓧⓨⓩ",
+	mathItalic: "𝒂𝒃𝒄𝒅𝒆𝒇𝒈𝒉𝒊𝒋𝒌𝒍𝒎𝒏𝒐𝒑𝒒𝒓𝒔𝒕𝒖𝒗𝒘𝒙𝒚𝒛",
+	mathSans: "𝖺𝖻𝖼𝖽𝖾𝖿𝗀𝗁𝗂𝗃𝗄𝗅𝗆𝗇𝗈𝗉𝗊𝗋𝗌𝗍𝗎𝗏𝗐𝗑𝗒𝗓",
+	mathSansBold: "𝗮𝗯𝗰𝗱𝗲𝗳𝗴𝗵𝗶𝗷𝗸𝗹𝗺𝗻𝗼𝗽𝗾𝗿𝘀𝘁𝘂𝘃𝘄𝘅𝘆𝘇",
+	monoSpace: "𝚊𝚋𝚌𝚍𝚎𝚏𝚐𝚑𝚒𝚓𝚔𝚕𝚖𝚗𝚘𝚙𝚚𝚛𝚜𝚝𝚞𝚟𝚠𝚡𝚢𝚣",
+	monoUpper: "ᴀʙᴄᴅᴇꜰɢʜɪᴊᴋʟᴍɴᴏᴘQʀꜱᴛᴜᴠᴡxʏᴢ",
+	scriptBoldItalic: "𝓪𝓫𝓬𝓭𝓮𝓯𝓰𝓱𝓲𝓳𝓴𝓵𝓶𝓷𝓸𝓹𝓺𝓻𝓼𝓽𝓾𝓿𝔀𝔁𝔂𝔃",
+};
--- Libs/Replaces.js
+++ Libs/Replaces.js
@@ -1,18 +1,23 @@
-// File://home/rose/BOT/SuryaRB/Libs/Replaces.js
-const aliases = {
-	wait: ["%name", "%tag"],
-	done: ["%name", "%tag", "%exec"],
+import { fonts, system } from "../Config/Fonts.js";
+
+const FONTS = {
+	system,
+	...fonts,
 };
 
 /**
- * @param {string} str
- * @param {string} alias
- * @param {{ sock: import("@whiskeysockets/baileys").WASocket, m: import("@whiskeysockets/baileys").proto.IWebMessageInfo & import("../Utils/Messages").ExtendedWAMessage }} options
+ * Replaces specific characters in a string with corresponding characters from a font.
+ * @param {string} str - The input string to be replaced.
+ * @param {string} [fontName="system"] - The name of the font to be used for replacement.
+ * @returns {string} - The modified string with replaced characters.
  */
-export default function Replace(str, alias, { sock, m }) {
-	if (typeof str !== "string" || !aliases[alias]) {
-		return str ?? "";
+export default function Replace(str, fontName = "system") {
+	const font = fonts[fontName];
+	if (font) {
+		const system = FONTS.system;
+		for (let i = 0; i < system.length; i++) {
+			str = String(str).replace(new RegExp(system[i], "g"), font[i]);
+		}
 	}
-	// we'll do later
 	return str;
 }
