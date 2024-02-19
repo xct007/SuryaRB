2fcd7fed02f598d924709aab53af7b87d65d3b0a
Nathajana <35220738+nat9h@users.noreply.github.com>
2024-02-19 09:35:24 +0000

    refactor turnMe.js

--- Message/Features/turnMe.js
+++ Message/Features/turnMe.js
@@ -12,49 +12,50 @@ export default {
 	group: false,
 	private: false,
 
-    execute: async function (m, { sock, api, text }) {
-        if (!text) {
-            return m.reply("Need text.");
-        }
-        const q = m.quoted ? m.quoted : m;
-        const mime = q.mtype || "";
-        if (!/image/g.test(mime)) {
-            return m.reply("Please reply/send a image with the command");
-        }
-        const media = await q.download();
-        const buffer = Buffer.isBuffer(media) ? media : Buffer.from(media, "utf-8");
-        const url = await telegraph(buffer);
-        const [style, prompt] = text.split(" ", 2);
-        if (!style || !prompt) {
-            return m.reply("Please provide both style and prompt.");
-        }
-        const { data } = await api.post("/image/turnMe", {
-            init_image: url,
-            style,
-            skin: "default",
-            image_num: 1,
-            prompt,
-            strength: 0.6
-        });
+	execute: async function (m, { sock, api, text }) {
+		if (!text) {
+			return m.reply("Need text.");
+		}
+		const q = m.quoted ? m.quoted : m;
+		const mime = q.mtype || "";
+		if (!/image/g.test(mime)) {
+			return m.reply("Please reply/send a image with the command");
+		}
+		const media = await q.download();
+		const buffer = Buffer.isBuffer(media) ? media : Buffer.from(media, "utf-8");
+		const url = await telegraph(buffer);
+		const [style, prompt] = text.split(" ", 2);
+		if (!style || !prompt) {
+			return m.reply("Please provide both style and prompt.");
+		}
+		const { data } = await api.post("/image/turnMe", {
+			init_image: url,
+			style,
+			skin: "default",
+			image_num: 1,
+			prompt,
+			strength: 0.6,
+		});
 
 		const { status, message, result, styles } = data;
 
 		if (!status) {
-            let count = 1,
-            extra_msg = "";
-          if (styles && Array.isArray(styles)) {
-            extra_msg += "\n_Here are the available styles:_\n\n";
-            for (const id of styles) {
-              extra_msg += `${count}. ${id}\n`;
-              count += 1;
-            }
-          }
-      
-          return m.reply(message + extra_msg);
+			if (styles && Array.isArray(styles)) {
+				let extra_msg =
+					"Available styles:\n\n" +
+					styles.map((style, index) => `${index + 1}. ${style}`).join("\n");
+				m.reply(extra_msg);
+			} else {
+				m.reply(message);
+			}
+			return;
 		}
 
-		await sock.sendMessage(m.chat, { image: { url: result.images } }, { quoted: m })
-
+		await sock.sendMessage(
+			m.chat,
+			{ image: { url: result.images } },
+			{ quoted: m }
+		);
 	},
 	failed: "Failed to execute the %cmd command\n%error",
 	wait: ["Please wait %tag", "Hold on %tag, fetching response"],
