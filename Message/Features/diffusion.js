19d09b65dd90009cc5aaaf325a09840a8403c912
Nathajana <35220738+nat9h@users.noreply.github.com>
2024-03-12 19:08:27 +0000

    small change

--- Message/Features/diffusion.js
+++ Message/Features/diffusion.js
@@ -1,60 +1,67 @@
-// File://home/rose/BOT/SuryaRB/Message/Features/chatbing.js
-import { telegraph } from "../../Libs/Uploader.js";
+import Uploader from "../../Libs/Uploader.js";
 
 export default {
-	command: ["diffusion", "diff"],
-	description: "All in One Diffusion.",
-	category: "Image",
-	owner: false,
-	admin: false,
-	hidden: false,
-	limit: false,
-	group: false,
-	private: false,
+  command: ["diffusion", "diff"],
+  description: "All in One Diffusion.",
+  category: "Image",
+  owner: false,
+  admin: false,
+  hidden: false,
+  limit: false,
+  group: false,
+  private: false,
 
-	execute: async function (m, { sock, api, text }) {
-		if (!text) {
-			return m.reply("Need text.");
-		}
+  execute: async function (m, { sock, api, text }) {
+    if (!text) {
+      m.reply("Please provide a prompt.");
+      return;
+    }
+    const q = m.quoted ? m.quoted : m;
+    const mime = q.mtype || "";
+    if (!/image/g.test(mime)) {
+      return m.reply("Please reply/send a image with the command");
+    }
+    const media = await q.download();
+    const buffer = Buffer.isBuffer(media) ? media : Buffer.from(media, "utf-8");
+    const url = await Uploader.providers.telegraph.upload(buffer);
+    m.replyUpdate("...", async (update) => {
+      const { data } = await api.post("/image/diffusion", {
+        prompt: text,
+        negative_prompt: "nsfw, blur",
+        sampler: "Euler a",
+        seed: -1,
+        ratio: "1:1",
+        style: "ACG",
+        init_image: url,
+        cfg: 7.5,
+        controlNet: "none",
+        image_num: 1,
+        steps: 25,
+      });
 
-		let initImage = null;
-		const quotedMessage = m.quoted ? m.quoted : m;
-		const mimeType = quotedMessage.mtype || "";
+      const { status, message, result } = data;
 
-		if (/webp|image|video|webm/g.test(mimeType)) {
-			const media = await quotedMessage.download();
-			const buffer = Buffer.isBuffer(media) ? media : Buffer.from(media, "utf-8");
-			initImage = await telegraph(buffer).catch(() => null);
-		}
+      if (!status) {
+        return m.reply(message);
+      }
 
-		const { data } = await api.post("/image/diffusion", {
-			prompt: text,
-			negative_prompt: "nsfw, blur",
-			sampler: "Euler a",
-			seed: -1,
-			ratio: "1:1",
-			style: "ACG",
-			init_image: initImage,
-			cfg: 7.5,
-			controlNet: "none",
-			image_num: 1,
-			steps: 25,
-		});
+      let metadataText = "";
+      for (const key in result.metadata) {
+        if (result.metadata.hasOwnProperty(key)) {
+          metadataText += `*${key}*: ${result.metadata[key]}\n`;
+        }
+      }
+      update(metadataText.trim());
 
-		const { status, message, result } = data;
+      await sock.sendMessage(
+        m.chat,
+        { image: { url: result.images } },
+        { quoted: m },
+      );
+    });
+  },
 
-		if (!status) {
-			return m.reply(message);
-		}
-
-		await sock.sendMessage(
-			m.chat,
-			{ image: { url: result.images } },
-			{ quoted: m }
-		);
-	},
-
-	failed: "Failed to execute the %cmd command\n%error",
-	wait: null,
-	done: null,
+  failed: "Failed to execute the %cmd command\n%error",
+  wait: null,
+  done: null,
 };
