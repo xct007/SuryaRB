7b3078186406a814b8d2aff4148d77e3c090f5ee
FrierenDv <hackerxct007@gmail.com>
2024-03-19 02:29:48 +0700

    TODOS

--- config.js
+++ config.js
@@ -1,14 +1,17 @@
 // File://home/rose/BOT/SuryaRB/config.js
 export const Config = {
-	phone_number: "628385818401",
-	owners: ["628385818401"],
+	phone_number: "6281278380339",
+	owners: ["628385818401", "+62 887-3133-561"],
 	use_pairing_code: true,
 	pairing_wait: 1000 * 6,
+
+	// TODO: FIX PREFIXES ERROR
 	prefix: ["!", ".", ""],
+
 	itsrose_apikey: process.env.ITSROSE_APIKEY || "Trial_Account_10K",
 
 	database: {
-		use_mongo: true,
+		use_mongo: false,
 		mongo_url: "mongodb://localhost:27017/database",
 
 		path: "./database.json",
