d6c878dcd39e6c6d99a9016c13a1b857d5be5a8b
Nathajana <35220738+nat9h@users.noreply.github.com>
2024-03-19 04:23:07 +0000

    add: kick member feature

---
+++ Message/Features/kick.js
@@ -0,0 +1,48 @@
+export default {
+	command: ["kick", "out"],
+	description: "Kick member from group.",
+	category: "Group",
+	owner: false,
+	group: true,
+	admin: true,
+	hidden: false,
+	limit: false,
+
+	/**
+	 * @param {import("../../Utils/Messages").ExtendedWAMessage} m - The message object.
+	 * @param {import("../Handler").miscOptions} options - The options.
+	 */
+	execute: async function (m, { sock, isBotAdmin, groupMetadata }) {
+		if (!isBotAdmin) {
+			return m.reply("I'm not an admin");
+		}
+
+		const user = m?.quoted?.sender || m.mentionedJid[0];
+		if (!user) {
+			return m.reply("Reply or tag a user");
+		}
+
+		const groupAdmins = groupMetadata?.participants
+			.filter((participant) => participant.admin)
+			.map((participant) => participant.id);
+
+		if (groupAdmins.includes(user)) {
+			return m.reply("You can't kick an admin");
+		}
+
+		await sock.sendMessage(
+			m.chat,
+			{
+				text: `Kicked @${user.replace(/[^0-9]/g, "")} from ${groupMetadata.subject}`,
+				mentions: [user],
+			},
+			{ quoted: m }
+		);
+
+		await sock.groupParticipantsUpdate(m.chat, [user], "remove").catch(() => {});
+	},
+
+	failed: "Failed to execute the %cmd command\n%error",
+	wait: null,
+	done: null,
+};
