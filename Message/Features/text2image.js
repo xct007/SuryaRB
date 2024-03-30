bbb3379547d8bd4d8a8de5de853eda15f2f44878
FrierenDv <hackerxct007@gmail.com>
2024-03-30 12:25:35 +0700

    update Features text2image

--- Message/Features/text2image.js
+++ Message/Features/text2image.js
@@ -1,75 +1,112 @@
 export default {
-  command: ["txt2img", "text2image"],
-  description: "Generate image with prompt using AI.",
-  category: "Image",
-  owner: false,
-  admin: false,
-  hidden: false,
-  limit: false,
-  group: false,
-  private: false,
+	command: ["txt2img", "text2image"],
+	description: "Generate image with prompt using AI.",
+	category: "Image",
+	owner: false,
+	admin: false,
+	hidden: false,
+	limit: false,
+	group: false,
+	private: false,
 
-  /**
-   * @param {import(../../Utils/Messages).ExtendedWAMessage} m - The message object.
-   * @param {import(../Handler).miscOptions}
-   */
-  execute: async function (m, { sock, api, text }) {
-    if (!text) {
-      m.reply("Please provide a prompt.");
-      return;
-    }
-    m.replyUpdate("...", async (update) => {
-      const { data } = await api.post("/image/diffusion/txt2img", {
-        server_name: "jisoo",
-        prompt: text,
-        negative_prompt:
-          "nsfw, bad anatomy, lowres, extra hands, extra legs, extra finger",
-        width: 512,
-        height: 512,
-        steps: 25,
-        model_id: "meinamix",
-        sampler: "UniPC",
-        cfg: 7.5,
-        seed: "",
-        enhance_prompt: "no",
-        multi_lingual: "no",
-        image_num: 1,
-        panorama: "no",
-        safety_checker: "no",
-        safety_checker_type: "blur",
-        lora_model: "",
-        lora_strength: 1,
-        clip_skip: 2,
-        embeddings_model: "",
-        webhook: "",
-      });
+	/**
+	 * @param {import(../../Utils/Messages).ExtendedWAMessage} m - The message object.
+	 * @param {import(../Handler).miscOptions}
+	 */
+	execute: async function (m, { sock, api, text }) {
+		if (!text) {
+			m.reply("Please provide a prompt.");
+			return;
+		}
 
-      const { status, result, message } = data;
+		const msg = await sock.sendMessage(
+			m.chat,
+			{ text: "_Preparing Txt2Img_" },
+			{ quoted: m }
+		);
 
-      if (!status) {
-        return update(message);
-      }
-      update(`Image generated in ${result.generation_time.toFixed(2)} seconds`);
-      let metadataText = "";
-      for (const key in result.metadata) {
-        if (result.metadata.hasOwnProperty(key)) {
-          metadataText += `*${key}*: ${result.metadata[key]}\n`;
-        }
-      }
-      setTimeout(() => {
-        update(metadataText);
-        setTimeout(async () => {
-          await sock.sendMessage(
-            m.chat,
-            { image: { url: result.images[0] } },
-            { quoted: m },
-          );
-        }, result.generation_time * 1000);
-      }, result.generation_time * 1000);
-    });
-  },
+		const { data } = await api.post("/image/diffusion/txt2img", {
+			// sd-api server name
+			server_name: "frieren",
 
-  failed: "Failed to execute the %cmd command\n%error",
-  wait: null,
-  done: null,
+			// positif and negative prompt
+			prompt: text,
+			negative_prompt: "(low quality:1.3), (worst quality:1.3), (monochrome:0.8)",
+
+			// Image w/h
+			width: 512,
+			height: 768,
+			image_num: 1,
+
+			// number of inference steps
+			steps: 25,
+
+			// Model and sampler
+			model_id: "yesmix",
+			sampler: "DPM++ 2M Karras",
+
+			// lora model and strength
+			// lora_model: "fashion-girl",
+			// lora_strength: "0.8",
+
+			// sd options
+			clip_skip: 2,
+			safety_checker_type: "blur",
+			cfg: 7,
+		});
+
+		const { status, result, message } = data;
+
+		if (!status) {
+			await sock.sendMessage(m.chat, {
+				text: message,
+				edit: { ...msg.key },
+			});
+			return;
+		}
+
+		await sock.sendMessage(m.chat, {
+			text: `Image generated in ${result["generation_time"].toFixed(2)} seconds`,
+			edit: { ...msg.key },
+		});
+
+		// wait the image to be ready
+		await new Promise((resolve) => {
+			setTimeout(() => {
+				resolve();
+			}, result["generation_time"] * 1000);
+		});
+
+		await sock.sendMessage(m.chat, {
+			text: (function (t = "") {
+				for (const key in result["metadata"]) {
+					t += `*${key}*: ${result["metadata"][key]}\n`;
+				}
+				return t.trim();
+			})(`*server_name*: ${result["server_name"]}\n`),
+			edit: { ...msg.key },
+		});
+
+		/** Loop. Can be useful if image_num more than 1 */
+		try {
+			for await (const url of result["images"]) {
+				await sock.sendMessage(
+					m.chat,
+					{
+						image: { url },
+					},
+					{ quoted: m }
+				);
+			}
+		} catch {
+			await sock.sendMessage(m.chat, {
+				text: "Failed getting images from: " + result["images"].join(", "),
+				edit: { ...msg.key },
+			});
+		}
+	},
+
+	failed: "Failed to execute the %cmd command\n%error",
+	wait: null,
+	done: null,
 };
