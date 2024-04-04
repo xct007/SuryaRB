b4d84ae70faf4c0a60237a53cf4af07e9aa26f4f
FrierenDv <hackerxct007@gmail.com>
2024-04-04 15:48:05 +0700

    Remove junk code, adjust the code to the new structure.

--- Utils/Messages.js
+++ Utils/Messages.js
@@ -1,10 +1,9 @@
-// File://home/rose/BOT/SuryaRB/Utils/Messages.js
 import {
 	getContentType,
 	jidNormalizedUser,
 	downloadContentFromMessage,
 } from "@whiskeysockets/baileys";
-import fs from "node:fs/promises";
+import fs from "fs/promises";
 
 import Replace from "../Libs/Replaces.js";
 import { mimeMap } from "./Medias.js";
@@ -29,7 +28,7 @@ const downloadMedia = async (message, pathFile) => {
 };
 
 /**
- * @typedef ExtendedWAMessage
+ * @typedef ExtendedWAMessage - The extended WAMessage object.
  * @property {string} chat - The chat JID.
  * @property {string} sender - The sender JID.
  * @property {boolean} isGroup - If the message is from a group.
@@ -37,19 +36,20 @@ const downloadMedia = async (message, pathFile) => {
  * @property {string} text - The message text.
  * @property {import("@whiskeysockets/baileys").proto.IMessageContextInfo} contextInfo - The context info.
  * @property {import("@whiskeysockets/baileys").WAMessage} quoted - The quoted message.
- * @property {() => Promise<Buffer | null>} download - Download the media and return the buffer.
+ * @property {(pathFile: string | undefined) => Promise<Buffer | string | Error>} download - Download the media and return the buffer or path.
  * @property {(text: string, font?: string) => void} reply - Reply to the message.
- * @property {Promise<(text: string, cb: (update: (n_text: string) => void) => void) => void>} replyUpdate - Update the message with a new text.
- * @property {() => void} delete
- * @property {(emoji: string) => void} react
- * @property {string[]} mentionedJid
+ * @property {(text: string, cb: (update: (text: string) => void) => void) => Promise<void>} replyUpdate - Reply to the message and update the message.
+ * @property {() => void} delete - Delete the message.
+ * @property {(emoji: string) => void} react - React to the message.
+ * @property {boolean} mentionMe - If the message mentions the bot.
  */
 
 /**
- * Process the incoming message and return an extended WAMessage object.
+ * Process an incoming message and return an extended WAMessage object.
+ *
  * @param {import("@whiskeysockets/baileys").BaileysEventMap["messages.upsert"]} upsert - The upsert event object.
  * @param {import("@whiskeysockets/baileys").WASocket} sock - The WASocket object.
- * @returns {import("@whiskeysockets/baileys").WAMessage & ExtendedWAMessage | null} - The extended WAMessage object.
+ * @returns {import("@whiskeysockets/baileys").WAMessage & ExtendedWAMessage | null} - The extended WAMessage object or null if invalid.
  */
 export function Messages(upsert, sock) {
 	const { messages } = upsert;
@@ -67,18 +67,20 @@ export function Messages(upsert, sock) {
 	if (m.message) {
 		m.mtype = getContentType(m.message);
 
-		// let's keep it nested
 		if (m.mtype === "ephemeralMessage") {
 			m.message = m.message[m.mtype].message;
 			m.mtype = getContentType(m.message);
-			if (m.mtype === "viewOnceMessageV2") {
-				m.message = m.message[m.mtype].message;
-				m.mtype = getContentType(m.message);
-			}
-		} else if (m.mtype === "viewOnceMessageV2") {
-			m.message = m.message[m.mtype].message;
-			m.mtype = getContentType(m.message);
 		}
+
+		if (
+			m.mtype === "viewOnceMessageV2" ||
+			m.mtype === "documentWithCaptionMessage"
+		) {
+			m.message = m.message[m.mtype].message;
+		}
+
+		m.mtype = getContentType(m.message);
+
 		try {
 			m.contextInfo = m.message[m.mtype]?.contextInfo || {};
 			m.mentionedJid = m.contextInfo?.mentionedJid || [];
@@ -86,32 +88,23 @@ export function Messages(upsert, sock) {
 			const quoted = m.contextInfo.quotedMessage || null;
 			if (quoted) {
 				if (quoted.ephemeralMessage) {
-					const tipe = Object.keys(quoted.ephemeralMessage.message)[0];
-					if (tipe === "viewOnceMessage") {
-						m.quoted = {
-							participant: jidNormalizedUser(m.contextInfo.participant),
-							message: quoted.ephemeralMessage.message.viewOnceMessage.message,
-						};
-					} else if (tipe === "protocolMessage") {
-						m.quoted = {
-							participant: jidNormalizedUser(m.contextInfo.participant),
-							message: quoted.ephemeralMessage.message.protocolMessage.message,
-						};
-					} else if (tipe === "viewOnceMessageV2") {
-						m.quoted = {
-							participant: jidNormalizedUser(m.contextInfo.participant),
-							message: quoted.ephemeralMessage.message.viewOnceMessageV2.message,
-						};
-					} else {
-						m.quoted = {
-							participant: jidNormalizedUser(m.contextInfo.participant),
-							message: quoted.ephemeralMessage.message,
-						};
-					}
-				} else {
+					const type = Object.keys(quoted.ephemeralMessage.message)[0];
+					const message =
+						type === "documentMessage"
+							? quoted.ephemeralMessage.message.documentMessage
+							: quoted.ephemeralMessage.message[type]?.message;
+
 					m.quoted = {
 						participant: jidNormalizedUser(m.contextInfo.participant),
-						message: quoted,
+						message: message || quoted.ephemeralMessage.message,
+					};
+				} else {
+					const type = Object.keys(quoted)[0];
+					const message = quoted[type]?.message;
+
+					m.quoted = {
+						participant: jidNormalizedUser(m.contextInfo.participant),
+						message: message || quoted,
 					};
 				}
 				m.quoted.sender = m.quoted.participant;
@@ -132,6 +125,8 @@ export function Messages(upsert, sock) {
 					remoteJid: m.chat,
 					...(m.isGroup ? { participant: m.contextInfo.participant } : {}),
 				};
+				m.quoted.mtype =
+					m.quoted.message[m.quoted.mtype]?.mimetype || getContentType(m.quoted.message);
 				m.quoted.react = (emoji) =>
 					sock.sendMessage(m.chat, {
 						react: {
@@ -144,12 +139,15 @@ export function Messages(upsert, sock) {
 			} else {
 				m.quoted = null;
 			}
+
 			m.text =
 				m.message[m.mtype]?.caption ||
 				m.message[m.mtype]?.text ||
 				m.message[m.mtype]?.conversation ||
 				m.message?.conversation ||
 				"";
+
+			m.mtype = m.message[m.mtype]?.mimetype || getContentType(m.message);
 			m.react = (emoji) =>
 				sock.sendMessage(m.chat, {
 					react: {
@@ -165,29 +163,19 @@ export function Messages(upsert, sock) {
 					},
 					{ quoted: m }
 				);
-			m.replyUpdate = (text, cb) => {
-				return new Promise(async (resolve) => {
-					const response = await sock.sendMessage(
-						m.chat,
-						{ text: String(text) },
-						{ quoted: m }
-					);
-					if (typeof cb === "function") {
-						/**
-						 * @param {string} n_text - The new text to update the message.
-						 * @returns {void}
-						 */
-						cb(async (n_text) => {
-							await sock.sendMessage(m.chat, {
-								text: String(n_text),
-								edit: response.key,
-							});
-							resolve();
-						});
-					}
-					resolve();
-				});
+			m.replyUpdate = async (text, cb) => {
+				const response = await sock.sendMessage(
+					m.chat,
+					{ text: String(text) },
+					{ quoted: m }
+				);
+				if (typeof cb === "function") {
+					cb(async (n_text) => {
+						await sock.sendMessage(m.chat, { text: String(n_text), edit: response.key });
+					});
+				}
 			};
+
 			m.delete = () => sock.sendMessage(m.chat, { delete: m.key });
 			m.download = (pathFile) => downloadMedia(m.message, pathFile);
 		} catch (error) {
