d1ccd224ea6dc63f1437f6ff441f2fb717c42fb2
Nathajana <35220738+nat9h@users.noreply.github.com>
2024-04-13 18:02:17 +0000

    Update Handler.js

--- Message/Handler.js
+++ Message/Handler.js
@@ -1,13 +1,11 @@
-// TODO: Refactor
-// File://home/rose/BOT/SuryaRB/Message/Handler.js
-import { performance } from "perf_hooks";
+// import { performance } from "perf_hooks";
 import Feature from "./Feature.js";
 import Queue from "../Libs/Queue.js";
 import { ApiRequest as api } from "../Utils/ApiRequest.js";
 import { Messages } from "../Utils/Messages.js";
 import { Prefix } from "../Utils/Prefix.js";
 import { Config } from "../config.js";
-import { Print } from "../Libs/Print.js";
+import { printMessage } from "../Libs/Print.js";
 
 import db from "../Libs/Database.js";
 
@@ -24,14 +22,17 @@ import db from "../Libs/Database.js";
  * @property {string} text - The text
  * @property {string} usedPrefix - The used prefix
  * @property {import("../Libs/Database").default} db - The database
+ * @property {typeof Feature.plugins} feature - The feature
+ * @property {ReturnType<import("../Utils/Store").Store>} store - The store
  */
 
 /**
  * Handles incoming messages
  * @param {import("@whiskeysockets/baileys").BaileysEventMap["messages.upsert"]} upsert - The upsert event
  * @param {import("@whiskeysockets/baileys").WASocket} sock - The socket connection
+ * @param {ReturnType<import("../Utils/Store").Store>} store - The store
  */
-export async function Handler(upsert, sock) {
+export async function Handler(upsert, sock, store) {
 	if (upsert.type !== "notify") {
 		return;
 	}
@@ -111,6 +112,7 @@ export async function Handler(upsert, sock) {
 				usedPrefix,
 				db,
 				feature: feature.plugins,
+				store,
 			};
 			if (plugin.before && typeof plugin.before === "function") {
 				try {
@@ -121,7 +123,9 @@ export async function Handler(upsert, sock) {
 			}
 
 			const isCostumPrefix = plugin?.customPrefix
-				? plugin.customPrefix.map((prefix) => prefix.toLowerCase()).includes(args[0])
+				? plugin.customPrefix
+						.map((prefix) => prefix.toLowerCase())
+						.includes(message.text.split(" ")[0].toLowerCase())
 				: false;
 			if (isCostumPrefix) {
 				args.shift();
@@ -136,29 +140,29 @@ export async function Handler(upsert, sock) {
 			if (canExecuteCommand) {
 				if (Queue.exist(message.sender, plugin)) {
 					message.reply("You are still using this command");
-					return;
+					continue;
 				}
 				if (plugin.owner && !isOwner) {
 					message.reply("Only the owner can use this command.");
-					return;
+					continue;
 				}
 				if (plugin.admin && message.isGroup && !isAdmin) {
 					message.reply("Only the admin can use this command.");
-					return;
+					continue;
 				}
 				if (plugin.group && !message.isGroup) {
 					message.reply("This command only available in group");
-					return;
+					continue;
 				}
 				if (plugin.private && message.isGroup) {
 					message.reply("This commnad only available in private chat");
-					return;
+					continue;
 				}
 
 				if (plugin.limit && !isOwner && !user.premium) {
 					if (user.limit < 0) {
 						message.reply("You have reached the limit of using this command");
-						return;
+						continue;
 					}
 					plugin.callback = () => {
 						user.limit--;
@@ -210,7 +214,7 @@ export async function Handler(upsert, sock) {
 					// 	);
 					// }
 				} catch (error) {
-					console.log(error, plugin);
+					console.error(error, plugin);
 					if (plugin.failed && typeof plugin.failed === "string") {
 						message.reply(
 							plugin.failed.replace("%cmd", command).replace("%error", String(error))
@@ -218,15 +222,18 @@ export async function Handler(upsert, sock) {
 					}
 				}
 			}
+
+			if (plugin.after && typeof plugin.after === "function") {
+				try {
+					await plugin.after(message, miscOptions);
+				} catch (error) {
+					console.error(error);
+				}
+			}
 		}
 	} catch (error) {
 		console.error(error);
 	}
 	Queue.remove(message.sender, executed_plugin);
-	Print.info(`From: ${message.sender}
-In: ${message.isGroup ? groupMetadata.subject : "Private Chat"}
-Type: ${message.mtype}
-RawText: ${message.text}
-Command: ${command ?? "No Command"}
-`);
+	printMessage(message, sock);
 }
