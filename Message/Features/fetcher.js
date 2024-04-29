c536f8f0498a0ef80b3370fd983c8d924121309a
FrierenDv <hackerxct007@gmail.com>
2024-04-29 17:13:06 +0700

    small changes

--- Message/Features/fetcher.js
+++ Message/Features/fetcher.js
@@ -1,5 +1,6 @@
 // File://home/rose/BOT/SuryaRB/Message/Features/fetcher.js
 import axios from "axios";
+import { to_audio } from "../../Libs/Converter.js";
 
 export default {
 	command: ["get", "fetch"],
@@ -12,6 +13,10 @@ export default {
 	group: false,
 	private: false,
 
+	/**
+	 * @param {import("../../Utils/Messages").ExtendedWAMessage} m - The message object.
+	 * @param {import("../Handler").miscOptions} options - The options.
+	 */
 	execute: async function (m, { sock, args }) {
 		const url = args[0];
 		if (!url) {
@@ -24,19 +29,16 @@ export default {
 			})
 			.catch((e) => e?.response);
 
-		if (!data || !headers) {
+		if (!data) {
 			return m.reply("Failed to fetch the url");
 		}
 
-		const mediaMap = {
-			image: "image",
-			video: "video",
-			audio: "audio",
-		};
+		const mediaMap = { image: "image", video: "video", audio: "audio" };
 
 		function getContentType() {
+			const contentType = headers?.["content-type"];
 			for (const key in mediaMap) {
-				if (headers?.["content-type"]?.includes(mediaMap[key])) {
+				if (contentType.includes(mediaMap[key])) {
 					return key;
 				}
 			}
@@ -45,9 +47,19 @@ export default {
 
 		const contentType = getContentType();
 		if (contentType) {
+			const content =
+				contentType === "audio"
+					? await to_audio(Buffer.from(data), "ogg")
+					: Buffer.from(data);
+
 			await sock.sendMessage(
 				m.chat,
-				{ [contentType]: Buffer.from(data) },
+				{
+					[contentType]: content,
+					...(contentType === "audio" && {
+						mimetype: "audio/mpeg",
+					}),
+				},
 				{ quoted: m }
 			);
 		} else {
