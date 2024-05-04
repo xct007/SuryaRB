6b1ba0b1ecf068fe2bd1197f069bcc1aa5428184
FrierenDv <hackerxct007@gmail.com>
2024-05-04 13:23:15 +0700

    adjust, using init_image or not

--- Message/Features/diffusion.js
+++ Message/Features/diffusion.js
@@ -11,54 +11,54 @@ export default {
 	group: false,
 	private: false,
 
-	execute: async function (m, { sock, api, text }) {
-		if (!text) {
+	execute: async function (m, { sock, api, text: prompt }) {
+		if (!prompt) {
 			m.reply("Please provide a prompt.");
 			return;
 		}
 		const q = m.quoted ? m.quoted : m;
 		const mime = q.mtype || "";
-		if (!/image/g.test(mime)) {
-			return m.reply("Please reply/send a image with the command");
+		const options = {
+			prompt,
+			negative_prompt: "nsfw, blur",
+			sampler: "Euler a",
+			seed: -1,
+			ratio: "1:1",
+			style: "ACG",
+			cfg: 7.5,
+			controlNet: "none",
+			image_num: 1,
+			steps: 25,
+		};
+		if (/image/g.test(mime)) {
+			const media = await q.download();
+			const buffer = Buffer.isBuffer(media) ? media : Buffer.from(media, "utf-8");
+			options["init_image"] = await Uploader.providers.telegraph.upload(buffer);
 		}
-		const media = await q.download();
-		const buffer = Buffer.isBuffer(media) ? media : Buffer.from(media, "utf-8");
-		const url = await Uploader.providers.telegraph.upload(buffer);
-		m.replyUpdate("...", async (update) => {
-			const { data } = await api.post("/image/diffusion", {
-				prompt: text,
-				negative_prompt: "nsfw, blur",
-				sampler: "Euler a",
-				seed: -1,
-				ratio: "1:1",
-				style: "ACG",
-				init_image: url,
-				cfg: 7.5,
-				controlNet: "none",
-				image_num: 1,
-				steps: 25,
-			});
-
-			const { status, message, result } = data;
-
-			if (!status) {
-				return m.reply(message);
-			}
-
-			let metadataText = "";
-			for (const key in result.metadata) {
-				if (result.metadata.hasOwnProperty(key)) {
-					metadataText += `*${key}*: ${result.metadata[key]}\n`;
-				}
-			}
-			update(metadataText.trim());
-
-			await sock.sendMessage(
-				m.chat,
-				{ image: { url: result.images } },
-				{ quoted: m }
-			);
+		const update = await m.replyUpdate("...");
+		const { data } = await api.post("/image/diffusion", {
+			...options,
 		});
+
+		const { status, message, result } = data;
+
+		if (!status) {
+			return update(message);
+		}
+
+		let metadataText = "";
+		for (const key in result.metadata) {
+			if (result.metadata.hasOwnProperty(key)) {
+				metadataText += `*${key}*: ${result.metadata[key]}\n`;
+			}
+		}
+		update(metadataText.trim());
+
+		await sock.sendMessage(
+			m.chat,
+			{ image: { url: result.images } },
+			{ quoted: m }
+		);
 	},
 
 	failed: "Failed to execute the %cmd command\n%error",
